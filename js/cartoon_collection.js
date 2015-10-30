ec15e99ba3ad6a0ed99ff56b6711809af50be3d3
Jeffrey Martín <jefdonmar@gmail.com>
2015-10-30 14:25:25 -0400

    set up my cartoon_collection_js file

---
+++ js/cartoon_collection.js
@@ -0,0 +1,18 @@
+import Backbone from 'backbone';
+import CartoonModel from './cartoon_model';
+
+let CartoonCollection = Backbone.Collection.extend({
+
+   url: 'https://api.parse.com/1/classes/cartoonList',
+
+   model: CartoonModel,
+
+   parse: function(data) {
+    return data.results;
+   }
+
+});
+
+export default CartoonCollection;
+
+// This sets up our list view of all of the cartoons that I have on my list 
