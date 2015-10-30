e2243d905d11b26d032fea97c5588dc6b46f8b69
Jeffrey Martín <jefdonmar@gmail.com>
2015-10-30 14:17:32 -0400

    Inital Commit + updated ajax_setup. js

---
+++ js/ajax_setup.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+
+const APP_ID = 'OMlIeJFoPoBWvSQxijVD1QWilp23C3I9uTvtii4W';
+const API_KEY = 'lJoeKAuCsm8wgKQKkxpcx5WBADBrdnf7DiXv4jyi';
+
+$.ajaxSetup({
+
+  headers: {
+
+    'X-Parse-Application-Id': APP_ID,
+    'X-Parse-REST-API-KEY': API_KEY
+  }
+
+
+});
